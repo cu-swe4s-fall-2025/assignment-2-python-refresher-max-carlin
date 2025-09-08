f2c8925c625a71fbe04d39ae500a62241175a5c9
Max Carlin <maca3369@colorado.edu>
2025-09-08 12:56:28 -0600

    Finished get_column function.

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,17 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    result = []
+
+    f = open(file_name, 'r')
+
+
+    for l in f: 
+        A = l.rstrip().split(',')
+
+        if A[query_column] == query_value: 
+            result.append(A[result_column])
+
+    f.close()
+    return result
+
+
+
