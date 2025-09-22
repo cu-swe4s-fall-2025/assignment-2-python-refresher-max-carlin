66a4d47c23bab50cba375c8e2b8db0d920d4490d
Max Carlin <maca3369@colorado.edu>
2025-09-22 16:53:36 -0600

    Updated print_fires to take command line arguments

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/_G_SdF8U)
-# python-refresher
-- Updated `get_column` in my_utils.py to grab a column from a csv file. 
-- `get_column` can take either an integer or a string for `result_column`. `result_column` defaults to 1.  
-- print_fires.py grabs all the forest fires for the United States and prints the CO$_2$ emissions caused by forest fires per year. 
-- run.sh runs print_fires.py
+# Printing Forest Fires
+
+> Prints CO2 emissions caused by forest fires in a given country.
+
+This software prints CO2 emissions caused by forest fires in a given country. `run.sh` calls on `print_fires` using command line arguments. It calls a `get_column` function that parses through a .csv file to return the column of interest and the fire emissions in that column. 
+
+## Installation
+
+## Usage Example
--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,52 @@
-from my_utils import get_column
+'''Printing CO2 Emissions
 
-country='United States of America'
-county_column = 0
-fires_column = 'Forest fires'
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column(file_name, county_column, country, fires_column)
-print(fires)
+This script allows the user to print the CO2 emissions caused by forest fires
+in any available country. It accepts .csv files. It requires a country,
+the column index of the country, the column header to extract from,
+and the path to the file to extract from.
+
+It contains the following custom function:
+    * get_column - returns a list of the column values of interest.
+'''
+
+from my_utils import get_column
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Prints forest fires given a country, a column number,"
+            "the name of the column, and the CSV file to pull from."),
+        prog='print_fires')
+
+    parser.add_argument('--country',
+                        type=str,
+                        help='Country of interest.',
+                        required=True)
+
+    parser.add_argument('--country_column',
+                        type=int,
+                        help='The column index of the country.',
+                        required=True)
+
+    parser.add_argument('--fires_column',
+                        type=str,
+                        help='Column header to extract from.',
+                        required=True)
+
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='The path to the file to extract from.',
+                        required=True)
+
+    args = parser.parse_args()
+
+    # Run the get_column function to retrieve the desired column.
+    fires = get_column(args.file_name,
+                       args.country_column,
+                       args.country,
+                       args.fires_column)
+
+    # Print the list of forrest fire CO2 emmissions.
+    print(fires)
